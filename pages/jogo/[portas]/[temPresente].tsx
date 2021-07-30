f38321ff30130d5f8e42ce8b489433a54f6a0851
Wilder Carvalho <wilder@motorola.com>
2021-07-30 19:53:13 -0300

    Fixing name of the jogo component to proper react language

--- pages/jogo/[portas]/[temPresente].tsx
+++ pages/jogo/[portas]/[temPresente].tsx
@@ -5,7 +5,7 @@ import styles from '../../../styles/Jogo.module.css'
 import Link from "next/link"
 import { useRouter } from "next/router";
 
-export default function jogo() {
+export default function Jogo() {
     const router = useRouter()
 
     const [portas, setPortas] = useState([])
